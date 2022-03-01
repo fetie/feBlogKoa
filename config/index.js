a9510b5cfefccb4d38c1fd9d92c74d53868a8ffe
baihongtie <a@fetie.cn>
2022-03-01 10:27:50 +0800

    feat: mongodb连接增加账号密码

--- config/index.js
+++ config/index.js
@@ -1,6 +1,11 @@
 
-const FILE_HOST='cdn.fetie.cn'
+const FILE_HOST='xx.cn'
+
+const DB_USER='xx'
+const DB_PASS='xx'
 
 module.exports = {
-    FILE_HOST
+    FILE_HOST,
+    DB_USER,
+    DB_PASS
 }
--- db/index.js
+++ db/index.js
@@ -1,7 +1,8 @@
 const mongoose=require('mongoose')
+const {DB_USER,DB_PASS} = require('../config/index')
 
 module.exports=()=>{
-    mongoose.connect('mongodb://127.0.0.1:27017/feBlog',{useUnifiedTopology:true,useNewUrlParser:true})
+    mongoose.connect('mongodb://127.0.0.1:27017/feBlog?authSource=admin',{useUnifiedTopology:true,useNewUrlParser:true,user:DB_USER,pass:DB_PASS})
         .then(()=>{
             console.log('连接成功')
         }).catch((err)=>{
